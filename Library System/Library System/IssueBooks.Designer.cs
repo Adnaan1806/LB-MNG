b090e4080cb94cc68974a7abfb67ee7bedaee5d7
MesQue1041 <abdulrahumanfazal@gmail.com>
2023-02-26 13:14:55 +0530

    just issue Book design

---
+++ Library System/Library System/IssueBooks.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace Library_System
+{
+    partial class IssueBooks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btn_refresh = new System.Windows.Forms.Button();
+            this.btn_issue = new System.Windows.Forms.Button();
+            this.txt_exit = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Member ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Book Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Book Issue Date";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(303, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(303, 165);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(140, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(303, 237);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(245, 22);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // btn_refresh
+            // 
+            this.btn_refresh.Location = new System.Drawing.Point(183, 312);
+            this.btn_refresh.Name = "btn_refresh";
+            this.btn_refresh.Size = new System.Drawing.Size(75, 23);
+            this.btn_refresh.TabIndex = 6;
+            this.btn_refresh.Text = "Refresh";
+            this.btn_refresh.UseVisualStyleBackColor = true;
+            // 
+            // btn_issue
+            // 
+            this.btn_issue.Location = new System.Drawing.Point(342, 312);
+            this.btn_issue.Name = "btn_issue";
+            this.btn_issue.Size = new System.Drawing.Size(106, 23);
+            this.btn_issue.TabIndex = 7;
+            this.btn_issue.Text = "Issue Book";
+            this.btn_issue.UseVisualStyleBackColor = true;
+            this.btn_issue.Click += new System.EventHandler(this.btn_issue_Click);
+            // 
+            // txt_exit
+            // 
+            this.txt_exit.Location = new System.Drawing.Point(521, 323);
+            this.txt_exit.Name = "txt_exit";
+            this.txt_exit.Size = new System.Drawing.Size(75, 23);
+            this.txt_exit.TabIndex = 8;
+            this.txt_exit.Text = "Exit";
+            this.txt_exit.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(433, 407);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(312, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Maximum 3 Books can be issued to one member";
+            // 
+            // IssueBooks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_exit);
+            this.Controls.Add(this.btn_issue);
+            this.Controls.Add(this.btn_refresh);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "IssueBooks";
+            this.Text = "IssueBooks";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btn_refresh;
+        private System.Windows.Forms.Button btn_issue;
+        private System.Windows.Forms.Button txt_exit;
+        private System.Windows.Forms.Label label4;
+    }
+}
