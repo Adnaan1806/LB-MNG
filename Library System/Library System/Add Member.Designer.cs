082b53ebb2ca47af5cc7e40b62ac0a142bed7ced
MesQue1041 <abdulrahumanfazal@gmail.com>
2023-03-03 14:03:54 +0530

    ADD MEMBERS DESIGN DONE

--- Library System/Library System/Add Member.Designer.cs
+++ Library System/Library System/Add Member.Designer.cs
@@ -30,6 +30,7 @@ namespace Library_System
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Add_Member));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -42,104 +43,133 @@ namespace Library_System
             this.TXT_Contact = new System.Windows.Forms.TextBox();
             this.TXT_Email = new System.Windows.Forms.TextBox();
             this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(173, 65);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Bauhaus 93", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 58);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 25);
+            this.label1.Size = new System.Drawing.Size(153, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "Member ID";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(173, 120);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Bauhaus 93", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 114);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(141, 25);
+            this.label2.Size = new System.Drawing.Size(194, 32);
             this.label2.TabIndex = 1;
             this.label2.Text = "Member Name";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(170, 177);
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Bauhaus 93", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 165);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(154, 25);
+            this.label3.Size = new System.Drawing.Size(218, 32);
             this.label3.TabIndex = 2;
             this.label3.Text = "Contact Number";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(173, 241);
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Bauhaus 93", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(21, 217);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 25);
+            this.label4.Size = new System.Drawing.Size(82, 32);
             this.label4.TabIndex = 3;
             this.label4.Text = "Email";
             // 
             // BTN_Clear
             // 
-            this.BTN_Clear.Location = new System.Drawing.Point(267, 357);
+            this.BTN_Clear.BackColor = System.Drawing.Color.DodgerBlue;
+            this.BTN_Clear.FlatAppearance.BorderSize = 0;
+            this.BTN_Clear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_Clear.ForeColor = System.Drawing.Color.White;
+            this.BTN_Clear.Location = new System.Drawing.Point(84, 299);
             this.BTN_Clear.Name = "BTN_Clear";
             this.BTN_Clear.Size = new System.Drawing.Size(98, 45);
             this.BTN_Clear.TabIndex = 4;
             this.BTN_Clear.Text = "Clear";
-            this.BTN_Clear.UseVisualStyleBackColor = true;
+            this.BTN_Clear.UseVisualStyleBackColor = false;
             this.BTN_Clear.Click += new System.EventHandler(this.BTN_Clear_Click);
             // 
             // BTN_Save
             // 
-            this.BTN_Save.Location = new System.Drawing.Point(381, 357);
+            this.BTN_Save.BackColor = System.Drawing.Color.DodgerBlue;
+            this.BTN_Save.FlatAppearance.BorderSize = 0;
+            this.BTN_Save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_Save.ForeColor = System.Drawing.Color.White;
+            this.BTN_Save.Location = new System.Drawing.Point(217, 299);
             this.BTN_Save.Name = "BTN_Save";
             this.BTN_Save.Size = new System.Drawing.Size(103, 45);
             this.BTN_Save.TabIndex = 5;
             this.BTN_Save.Text = "Save Details";
-            this.BTN_Save.UseVisualStyleBackColor = true;
+            this.BTN_Save.UseVisualStyleBackColor = false;
             this.BTN_Save.Click += new System.EventHandler(this.BTN_Save_Click);
             // 
             // BTN_Exit
             // 
-            this.BTN_Exit.Location = new System.Drawing.Point(499, 357);
+            this.BTN_Exit.BackColor = System.Drawing.Color.DodgerBlue;
+            this.BTN_Exit.FlatAppearance.BorderSize = 0;
+            this.BTN_Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_Exit.ForeColor = System.Drawing.Color.White;
+            this.BTN_Exit.Location = new System.Drawing.Point(353, 299);
             this.BTN_Exit.Name = "BTN_Exit";
             this.BTN_Exit.Size = new System.Drawing.Size(94, 45);
             this.BTN_Exit.TabIndex = 6;
             this.BTN_Exit.Text = "Exit";
-            this.BTN_Exit.UseVisualStyleBackColor = true;
+            this.BTN_Exit.UseVisualStyleBackColor = false;
             this.BTN_Exit.Click += new System.EventHandler(this.BTN_Exit_Click);
             // 
             // TXT_id
             // 
-            this.TXT_id.Location = new System.Drawing.Point(351, 65);
+            this.TXT_id.Location = new System.Drawing.Point(279, 58);
             this.TXT_id.Name = "TXT_id";
-            this.TXT_id.Size = new System.Drawing.Size(189, 22);
+            this.TXT_id.Size = new System.Drawing.Size(214, 22);
             this.TXT_id.TabIndex = 7;
             // 
             // TXT_Name
             // 
-            this.TXT_Name.Location = new System.Drawing.Point(351, 120);
+            this.TXT_Name.Location = new System.Drawing.Point(279, 114);
             this.TXT_Name.Name = "TXT_Name";
-            this.TXT_Name.Size = new System.Drawing.Size(189, 22);
+            this.TXT_Name.Size = new System.Drawing.Size(214, 22);
             this.TXT_Name.TabIndex = 8;
             // 
             // TXT_Contact
             // 
-            this.TXT_Contact.Location = new System.Drawing.Point(351, 177);
+            this.TXT_Contact.Location = new System.Drawing.Point(279, 165);
             this.TXT_Contact.Name = "TXT_Contact";
-            this.TXT_Contact.Size = new System.Drawing.Size(189, 22);
+            this.TXT_Contact.Size = new System.Drawing.Size(214, 22);
             this.TXT_Contact.TabIndex = 9;
             // 
             // TXT_Email
             // 
-            this.TXT_Email.Location = new System.Drawing.Point(351, 241);
+            this.TXT_Email.Location = new System.Drawing.Point(279, 217);
             this.TXT_Email.Name = "TXT_Email";
-            this.TXT_Email.Size = new System.Drawing.Size(189, 22);
+            this.TXT_Email.Size = new System.Drawing.Size(214, 22);
             this.TXT_Email.TabIndex = 10;
             // 
             // bunifuDragControl1
@@ -149,28 +179,117 @@ namespace Library_System
             this.bunifuDragControl1.TargetControl = null;
             this.bunifuDragControl1.Vertical = true;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.panel8);
+            this.panel1.Controls.Add(this.panel7);
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.TXT_Email);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.TXT_Contact);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.TXT_Name);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.TXT_id);
+            this.panel1.Controls.Add(this.BTN_Clear);
+            this.panel1.Controls.Add(this.BTN_Exit);
+            this.panel1.Controls.Add(this.BTN_Save);
+            this.panel1.Location = new System.Drawing.Point(309, 144);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(511, 379);
+            this.panel1.TabIndex = 11;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Location = new System.Drawing.Point(0, -3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(841, 124);
+            this.panel3.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bauhaus 93", 31.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(389, 62);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "ADD MEMBERS";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Location = new System.Drawing.Point(228, 15);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(415, 100);
+            this.panel4.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Black;
+            this.panel5.Location = new System.Drawing.Point(279, 80);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(214, 1);
+            this.panel5.TabIndex = 11;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Black;
+            this.panel6.Location = new System.Drawing.Point(280, 136);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(214, 1);
+            this.panel6.TabIndex = 12;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Black;
+            this.panel7.Location = new System.Drawing.Point(280, 187);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(214, 1);
+            this.panel7.TabIndex = 13;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.Black;
+            this.panel8.Location = new System.Drawing.Point(279, 239);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(214, 1);
+            this.panel8.TabIndex = 14;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(25, 181);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(264, 258);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // Add_Member
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.TXT_Email);
-            this.Controls.Add(this.TXT_Contact);
-            this.Controls.Add(this.TXT_Name);
-            this.Controls.Add(this.TXT_id);
-            this.Controls.Add(this.BTN_Exit);
-            this.Controls.Add(this.BTN_Save);
-            this.Controls.Add(this.BTN_Clear);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(840, 535);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
             this.Name = "Add_Member";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Add_Member";
             this.Load += new System.EventHandler(this.Add_Member_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -188,5 +307,14 @@ namespace Library_System
         private System.Windows.Forms.TextBox TXT_Contact;
         private System.Windows.Forms.TextBox TXT_Email;
         private Bunifu.Framework.UI.BunifuDragControl bunifuDragControl1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
